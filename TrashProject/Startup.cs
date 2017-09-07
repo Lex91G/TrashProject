348781b071c8a50c7df566f58851e2a32058179c
Your Name <you@example.com>
2017-09-07 15:35:40 -0500

    Got client and employee seperated still alot to do

---
+++ TrashProject/Startup.cs
@@ -0,0 +1,52 @@
+﻿
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+using TrashProject.Models;
+
+[assembly: OwinStartupAttribute(typeof(TrashProject.Startup))]
+namespace TrashProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+
+        // In this method we will create default User roles and Admin user for login   
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // creating Creating Manager role    
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Employee role    
+            if (!roleManager.RoleExists("Client"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Client";
+                roleManager.Create(role);
+
+            }
+        }
+
+    }
+}
+
